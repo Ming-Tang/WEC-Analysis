eae79733be26d4ff65bf91fc0c5a2faec51387cc
Ming-Tang <project.shinkirou@gmail.com>
2016-07-02 21:52:35 -0700

    Reorganize Report_Interactive.Rmd

--- WEC_Races.R
+++ WEC_Races.R
@@ -1,15 +1,23 @@
-analyze_events <- TRUE
-delayedAssign("Le_Mans_2015", WEC.Analysis(2015, "Le_Mans", analyze_events))
-delayedAssign("Silverstone_2016", WEC.Analysis(2016, "Silverstone", analyze_events))
-delayedAssign("Spa_2016", WEC.Analysis(2016, "Spa", analyze_events))
-delayedAssign("Le_Mans_2016", WEC.Analysis(2016, "Le_Mans", analyze_events))
-
 race_list <- c(
   "Le_Mans_2015",
   "Silverstone_2016",
   "Spa_2016",
   "Le_Mans_2016"
 )
+
+load_race <- function(year, race) WEC.Analysis(year, race, TRUE)
+
+if (!file.exists("WEC_Races.Rdata")) {
+  delayedAssign("Le_Mans_2015", load_race(2015, "Le_Mans"))
+  delayedAssign("Silverstone_2016", load_race(2016, "Silverstone"))
+  delayedAssign("Spa_2016", load_race(2016, "Spa"))
+  delayedAssign("Le_Mans_2016", load_race(2016, "Le_Mans"))
+  
+  save(list=race_list, file="WEC_Races.Rdata")
+} else {
+  load("WEC_Races.Rdata")  
+}
+
 
 delayedAssign("choices", {
   choices <- as.list(race_list)
