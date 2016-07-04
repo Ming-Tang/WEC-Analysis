8f872467c6a73b8c474fa59a1f7843f2ea19a1d1
Ming-Tang <project.shinkirou@gmail.com>
2016-07-04 13:04:14 -0700

    Include starting grid, and doubled the speed of generating lap charts for free!

--- Lap_Chart.R
+++ Lap_Chart.R
@@ -6,7 +6,8 @@ library(profvis)
 Race <- Le_Mans.2016
 Grid <- Race$Grid
 Events <- as.data.table(Race$Events)
-setkey(Events, Lap, Time)
+setkey(Events, Time)
+setkey(Events, Lap, Sector)
 
 #sectors <- c(0L)
 sectors <- c(0L,1L,2L)
@@ -14,23 +15,23 @@ sectors <- c(0L,1L,2L)
 n_sectors <- length(sectors)
 n_laps <- max(Events$Lap)
 n_cars <- length(levels(Events$Car.Number))
-Lap.Chart <- matrix(NA, nrow=1L + n_sectors * (n_laps+1), ncol=n_cars)
-rownames(Lap.Chart) <- c("Grid", sapply(1L:(n_laps+1), function(i) paste(i, sectors, sep=".")))
+Lap.Chart <- matrix(NA, nrow=n_sectors * (n_laps+1), ncol=n_cars)
+rnames <- sapply(0L:n_laps, function(i) paste(i, sectors, sep="."))
+rnames[1] <- "Grid"
+rownames(Lap.Chart) <- rnames
 colnames(Lap.Chart) <- 1L:n_cars
 
-get_index <- function(lap, sector) 2 + (lap - 1) * n_sectors + sector
-get_lap_from_index <- function(index) 1 + (index - 2) %/% n_sectors
-get_sector_from_index <- function(index) if (index == 1) 1 else index - get_index(get_lap_from_index(index), 0)
-get_fractional_lap <- function(index) pmax(0, (index - 2) / n_sectors + 1)
+get_index <- function(lap, sector) lap * n_sectors + sector + 1
+get_lap_from_index <- function(index) (index - 1) %/% n_sectors
+get_sector_from_index <- function(index) (index - 1) %% n_sectors
+get_fractional_lap <- function(index) (index - 1) / n_sectors
 
-Lap.Chart[1,] <- as.character(Grid$Car.Number)
-
-for (lap in 1L:n_laps) {
+for (lap in 0L:n_laps) {
   for (sector in sectors) {
-    car_numbers_0 <- Events[J(lap)][Sector==sector, as.character(Car.Number)]
+    car_numbers_0 <- Events[.(lap, sector), as.character(Car.Number)]
     car_numbers <- unique(car_numbers_0, fromLast=TRUE)
     #stopifnot(length(unique(car_numbers)) == length(car_numbers))
-    if (length(car_numbers) != length(car_numbers_0)) {
+    if (FALSE && length(car_numbers) != length(car_numbers_0)) {
       print(sprintf("lap=%d sector=%d", lap, sector))
       print("*** car_numbers_0: ")
       print(car_numbers_0)
@@ -40,16 +41,19 @@ for (lap in 1L:n_laps) {
       print(Events[Lap==lap & Sector==sector, Time])
       print("")
     }
-    #times <- Events[Lap==lap & Sector==sector, Time]
-    #stopifnot(all.equal(sort(times), times))
-    #stopifnot(n_cars - length(car_numbers) >= 0)
+    if (FALSE) {
+      times <- Events[.(lap, sector), Time]
+      stopifnot(all.equal(sort(times), times))
+      stopifnot(n_cars - length(car_numbers) >= 0)
+    }
     Lap.Chart[get_index(lap, sector),] <- c(car_numbers, rep(NA, n_cars - length(car_numbers)))
   }
 }
-View(t(Lap.Chart))
+Lap.Chart[1,] <- as.character(Grid$Car.Number)
 
 #})
 
+View(t(Lap.Chart))
 DT <- data.table(Index=integer(n), Pos=integer(n), Car.Number=character(n))
 i <- 1L
 xs <- 1L:dim(Lap.Chart)[1L]
