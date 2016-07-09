651977dc20bdfd08e6f586d097a1e2e4d672e9b5
Ming-Tang <project.shinkirou@gmail.com>
2016-07-09 10:36:35 -0700

    Analysis of pitstops

--- Pit_Analysis.R
+++ Pit_Analysis.R
@@ -1,33 +1,51 @@
-Race <- Le_Mans.2016
-Pit.Analysis <- as.data.table(Race$Analysis)[,.(NUMBER, DRIVER_NUMBER, TEAM, CLASS, DRIVER_NAME, LAP_NUMBER, Lap.Time, Time, Pit.Time)]
-setnames(Pit.Analysis,
-         c("NUMBER", "DRIVER_NUMBER", "TEAM", "CLASS", "DRIVER_NAME", "LAP_NUMBER"),
-         c("Car.Number", "Driver.Number", "Team", "Class", "Driver.Name", "Lap"))
-Pit.Analysis[, `:=`(Pit = !is.na(Pit.Time) & Pit.Time > 1e-2, Time = Time - Lap.Time)]
-Pit.Analysis <- Pit.Analysis[(Pit)]
-setkey(Pit.Analysis, Car.Number, Time)
-Pit.Analysis[, Driver.Number := as.integer(Driver.Number)]
-Pit.Analysis[, Driver.Change := c(FALSE, diff(Driver.Number) != 0), by=Car.Number]
-Pit.Analysis[, `:=`(
-  Stint = cumsum(Pit),
-  Stint.Length = c(0, diff(Lap)),
-  Changes = 1+cumsum(Driver.Change),
-  Total.Pit.Time = cumsum(ifelse(Pit, Pit.Time, 0))
-), by=Car.Number]
-Pit.Analysis[,`:=`(Lap.Time = NULL, Pit = NULL)]
+library(ggplot2)
+library(data.table)
 
-Pit.Summary <- Pit.Analysis[,.(
-  Team=Team[1], Class=Class[1], Laps=max(Lap), 
-  Stint=max(Stint), Changes=max(Changes), Total.Pit.Time=max(Total.Pit.Time)
-), keyby=Car.Number]
-Pit.Summary[, `:=`(
-  Laps.Stint=Laps/Stint,
-  Laps.Driver=Laps/Changes,
-  Pit.Time.Lap = Total.Pit.Time/Laps
-)]
-View(Pit.Analysis)
-View(Pit.Summary)
-
-ggplot(Pit.Analysis, aes(x=Time/3600,y=Car.Number,col=Class,label=Lap)) +
-  geom_errorbarh(aes(height=ifelse(Driver.Change,0.7,0.2),xmin=Time/3600,xmax=(Time+Pit.Time)/3600, yend=Car.Number)) +
-  plot_styles$color_by_class + xlab("Hour") + scale_x_continuous(breaks=0:24, minor_breaks=seq(0, 24, 1/6))
+Analyze.Pitstops <- function(Race) {
+  Pit.Analysis <- as.data.table(Race$Analysis)
+  Pit.Analysis <- merge(as.data.table(Race$Classification)[, NUMBER := as.factor(NUMBER)], Pit.Analysis, by="NUMBER", suffixes=c(".C", ""))
+  
+  setnames(Pit.Analysis,
+           c("NUMBER", "DRIVER_NUMBER", "TEAM", "VEHICLE", "TYRES", "CLASS", "GROUP", "DRIVER_NAME", "LAP_NUMBER"),
+           c("Car.Number", "Driver.Number", "Team", "Vehicle", "Tyres", "Class", "Group", "Driver.Name", "Lap"))
+  
+  Pit.Analysis <- Pit.Analysis[,.(Car.Number, Driver.Number, Team, Vehicle, Tyres, Class, Group, Lap, Lap.Time, Time, Pit.Time)]
+  Pit.Analysis[, `:=`(Pit = !is.na(Pit.Time) & Pit.Time > 1e-2, Time = Time - Lap.Time)][,Roll.Time := Time]
+  Pit.Analysis <- Pit.Analysis[(Pit)]
+  
+  setkey(Pit.Analysis, Car.Number, Roll.Time)
+  
+  Pit.Analysis[, Driver.Number := as.integer(Driver.Number)]
+  Pit.Analysis[, Driver.Change := c(FALSE, diff(Driver.Number) != 0), by=Car.Number]
+  Pit.Analysis[, `:=`(
+    Stint = cumsum(Pit),
+    Stint.Length = c(0, diff(Lap)),
+    Changes = 1+cumsum(Driver.Change),
+    Total.Pit.Time = cumsum(ifelse(Pit, Pit.Time, 0))
+  ), by=Car.Number]
+  Pit.Analysis[,`:=`(Lap.Time = NULL, Pit = NULL)]
+  
+  Pit.Summary <- Pit.Analysis[,.(
+    Team=Team[1], Class=Class[1], Laps=max(Lap), 
+    Stints=max(Stint), Changes=max(Changes), Total.Pit.Time=max(Total.Pit.Time)
+  ), keyby=Car.Number]
+  
+  Total.Times <- as.data.table(Race$Classification)[
+    ,.(Car.Number=as.factor(NUMBER),
+       Fastest.Lap = Rcpp_apply_parse_timing(gsub("'", ":", Race$Classification$FL_TIME)),
+       Total.Time = Rcpp_apply_parse_timing(gsub("'", ":", Race$Classification$TOTAL_TIME)),
+       Classified=STATUS == "Classified", Position=POSITION)
+  ]
+  Pit.Summary <- Pit.Summary[Total.Times][, Race.Time := Total.Time - Total.Pit.Time]
+  
+  Pit.Summary[, `:=`(
+    Laps.Stint = Laps/Stints,
+    Laps.Driver = Laps/Changes,
+    Pit.Time.Lap = Total.Pit.Time/Laps,
+    Pit.Time.Ratio = Total.Pit.Time/Race.Time,
+    Avg.Lap = Race.Time/Laps
+  )]
+  #View(Pit.Analysis)
+  #View(Pit.Summary)
+  list(Pit.Analysis=Pit.Analysis, Pit.Summary=Pit.Summary)
+}
